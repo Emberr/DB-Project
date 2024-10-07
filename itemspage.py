5dc05bee16b8c14dec5c23bd76a84c164da8e794
Zane <z.zangana@student.maastrichtuniversity.nl>
2024-10-07 23:16:58 +0200

    added items screen

--- itemspage.py
+++ itemspage.py
@@ -1,8 +1,15 @@
-from flask import Blueprint, render_template, session, flash, redirect, url_for, request
-from database import get_db_connection
-from TABLES import Customer, Session
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from TABLES import Pizza, Drink, Ingredient, Dessert
 
 items_pointer = Blueprint('items', __name__)
+
+# Create an engine
+engine = create_engine('mysql+pymysql://root:qazWSX123%21%40%23@localhost/PDS')
+
+# Create a configured "Session" class
+Session = sessionmaker(bind=engine)
 
 @items_pointer.route('/items')
 def items():
@@ -11,4 +18,15 @@ def items():
         flash('Please log in first.')
         return redirect(url_for('login.login'))
 
-    return f"Item page for {username} - Coming soon!"
+    # Create a session
+    session = Session()
+
+    # Fetch items from the database
+    pizzas = session.query(Pizza).all()
+    drinks = session.query(Drink).all()
+    desserts = session.query(Dessert).all()
+
+    # Close the session
+    session.close()
+
+    return render_template('items.html', username=username, pizzas=pizzas, drinks=drinks, desserts=desserts)
